cfc6f704cd14d1f3e71c273d551476ae7268ad02
Sami <bmb,sami@yahoo.com>
2019-03-11 21:29:54 +0200

    Starting Tamagotchi gane

---
+++ src/main/Java/org/fasttrackit/catel/TamagotchiGame.java
@@ -0,0 +1,24 @@
+package org.fasttrackit.catel;
+
+public class TamagotchiGame {
+
+    public static void main(String[] args) {
+
+
+        Catel dog1 = new Catel();
+        dog1.setName("Rex");
+
+
+        Catel dog2 = new Catel();
+        dog2.setName("Rufus");
+
+
+        for(int zi=0;zi<15;zi++) {
+
+            dog2.eating();
+
+        }
+        System.out.println("Catelul "+" "+dog2.getName()+" are varsta de " +dog2.getAge()+ " ani.");
+
+    }
+}
