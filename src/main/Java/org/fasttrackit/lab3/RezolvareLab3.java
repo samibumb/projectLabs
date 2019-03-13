0adadb266d369422eb1818d092a337903742be48
Sami <bmb,sami@yahoo.com>
2019-03-13 12:39:01 +0200

    Starting Tema Lab3

--- src/main/Java/org/fasttrackit/lab3/RezolvareLab3.java
+++ src/main/Java/org/fasttrackit/lab3/RezolvareLab3.java
@@ -10,8 +10,8 @@ public class RezolvareLab3 {
         //Sa se calculeze media aritmetica a doua numere citite de la tastatura
 
         Scanner sc = new Scanner(System.in) ;
-        double a ,b  ;
-        double mAritmetica;
+        double a,b,mAritmetica;
+
         System.out.print("a este :");
         a=sc.nextDouble();
 
@@ -21,9 +21,23 @@ public class RezolvareLab3 {
         mAritmetica=(a+b)/2 ;
         System.out.println("Media aritmetica este egala cu : "+mAritmetica);
 
-
-
-
-    }
+    interval(8);
 
     }
+    //Exercitiul 13
+
+    public static void interval(int x) {
+
+        if (x > 4 && x < 15) {
+            System.out.println(x + " apartine intervalului [4,15]");
+
+        } else {
+            System.out.println(x + " este in afara intervalului [4,15]");
+        }
+    }
+
+
+
+}
+
+
