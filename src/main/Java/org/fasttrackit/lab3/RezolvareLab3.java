5f6cd4f6545b5fe340f2751e1734dcbf0f51891c
Sami <bmb,sami@yahoo.com>
2019-03-13 14:01:18 +0200

    Starting Tema Lab3

--- src/main/Java/org/fasttrackit/lab3/RezolvareLab3.java
+++ src/main/Java/org/fasttrackit/lab3/RezolvareLab3.java
@@ -22,6 +22,10 @@ public class RezolvareLab3 {
         System.out.println("Media aritmetica este egala cu : "+mAritmetica);
 
     interval(8);
+    inversul(789,0);
+    palindrom(3553,0,0);
+    sumaInt(123,0);
+
 
     }
     //Exercitiul 13
@@ -36,8 +40,48 @@ public class RezolvareLab3 {
         }
     }
 
+        //ex 7
 
+    public static void inversul(int x , int inv ){
+
+        while(x!=0) {
+            inv = inv * 10 + x % 10;
+            x=x/10 ;
+
+        }
+            System.out.println("inversul numarului este : "+inv);
+    }
+
+   //Exercitiul 8
+
+    public static void palindrom(int x , int invers , int palindrom){
+        palindrom=x ;
+        while(x!=0){
+            invers=invers*10+x%10 ;
+            x=x/10 ;
+        }
+        if(invers==palindrom){
+            System.out.println("numarul este un palindrom");
+        }else{
+            System.out.println("numarul nu este un palindrom");
+        }
+    }
+
+    //Exercitiul 11
+
+    public static void sumaInt(int x,int sum) {
+
+        //suma nr unui numar
+        while (x!=0){
+            sum=sum+x%10;
+            x=x/10;
+        }
+        System.out.println("Suma este : " +sum);
+
+    }
+     
 
 }
+
 
 
